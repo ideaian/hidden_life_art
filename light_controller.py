64d50d3a4c32c02309ccb08b99e5636a2cadc13c
Ian Derrington <ian.derrington@gmail.com>
2018-11-11 09:34:15 -0800

    going to change it so that the gpio_writer uses the color designer

--- light_controller.py
+++ light_controller.py
@@ -5,20 +5,19 @@ import numpy as np
 import threading
 from killable_threads import ThreadWithExc
 
-COLOR_MAP = {'r': [1, 0, 0], 'g':[0, 1, 0], 'b': [0, 0, 1]}
 red = [1, 0, 0]
 green = [0, 1, 0]
 blue = [0, 0, 1]
+COLOR_MAP = {'r': red, 'g': green, 'b': blue}
 N_COLOR = 3
 GPIO_MODES = {'bcm': GPIO.BCM, 'board': GPIO.BOARD}
 
 class LightController(object):
-    def __init__(self, pinout, gpio_mode):
+    def __init__(self, pinout, gpio_mode='bcm'):
         if gpio_mode not in GPIO_MODES.keys():
             msg = 'Mode {} not available in list of {}'.format(gpio_modes.keys())
             raise ValueError(msg)
         gpio_mode = GPIO_MODES[gpio_mode]
-        GPIO.setmode(GPIO.BCM)
         GPIO.setmode(gpio_mode)
         self.pinout= pinout
         self.color_matrix = pinout * 0
@@ -26,16 +25,29 @@ class LightController(object):
         self.zero_mat = GPIO.LOW * np.ones(shape=pinout.shape, dtype=int)
         self.one_mat = GPIO.HIGH * np.ones(shape=pinout.shape, dtype=int)
         self.init_pinout()
+        self.KBI = [False]
 
     def init_pinout(self):
         for pin in iter(self.pinout.flat):
             GPIO.setup(pin, GPIO.OUT)
     
     def gpio_writer(self):
-
-        print('calling writer')
-        for pin, intensity in zip(self.pinout.flat, self.color_matrix.flat):
-            GPIO.output(pin, intensity)
+        while True:
+            i=0
+            try:
+                i+=1
+                if i%1000==0:
+                    print("writer")
+                for pin, intensity in zip(self.pinout.flat, self.color_matrix.flat):
+                    GPIO.output(pin, intensity)
+                
+                if self.KBI[0]:
+                    print("external interrupt")
+                    break
+            except KeyboardInterrupt:
+                print("Interrupt in gpio writer")
+                return
+                
     
     def __exit__(self, exc_type=None, exc_value=None, traceback=None):
 	print("Cleaning up on ex_type")
@@ -65,7 +77,7 @@ class ColorFromGlobalWriter(LightController):
         
         #: this is where I would append a listener thread.
         designer_thread = ThreadWithExc(name='designer',target=self.color_designer, 
-                                     args=(self.color_matrix, self.color_designer_args))
+                                     args=(self.color_matrix, self.KBI, self.color_designer_args))
         #designer_thread.daemon = True
         self.threads.append(designer_thread)
         writer_thread = ThreadWithExc(name='writer', 
@@ -78,7 +90,7 @@ class ColorFromGlobalWriter(LightController):
         for t in self.threads:
             print("thread {} started".format(t.name))
             t.start()
-        print('started')
+        print("all threads started")
         #for t in self.threads:
         #    print("thread {} joined".format(t.name))
         #    t.join()
--- new_blink.py
+++ new_blink.py
@@ -31,18 +31,32 @@ def simple_test(color_matrix=None):
 
 
 # make_color matrix and get_args can be bundled into a general class
-def make_color_matrix(color_mat, color):
+def make_color_matrix(color_mat, KBI, color):
+    time_on = .001
+    time_off = .001
     n_lights = color_mat.shape[0]
     color = color['color_designer_args']
+    print('Starting make color')
     if isinstance(color, str):
         color = COLOR_MAP[color]
     while True:
         try:
+            i=0
+            if i%1000==0:
+                print("make_color_matrix {}".format(color))
             for light_ndx in range(n_lights):
                 for color_val, colr_ndx in enumerate(color):
                     color_mat[light_ndx, colr_ndx] = GPIO.HIGH * color_val
+            time.sleep(time_on)
+
+            for light_ndx in range(n_lights):
+                for color_val, colr_ndx in enumerate(color):
+                    color_mat[light_ndx, colr_ndx] = GPIO.LOW #* color_val
+            time.sleep(time_off)
         except KeyboardInterrupt:
-            raise StandardError
+            print("Interrupt in MakeColorMatrix")
+            KBI[0] = True
+            return
 
 def get_args():
     import sys
@@ -67,6 +81,8 @@ def main():
             color_designer_args=color_designer_args
             )
     gcw.initialize_threads() 
+    gcw.start_threads() 
+    '''
     try:
         gcw.start_threads() 
     except (KeyboardInterrupt, SystemExit):
@@ -75,6 +91,7 @@ def main():
         print("Cleaning up")
         gcw.__exit__()
 
+    '''
 
 if __name__ == '__main__':
     main()

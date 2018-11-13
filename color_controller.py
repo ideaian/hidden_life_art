a566eb784a1f7f24615c0158706fbd3dc5218b4d
Ian Derrington <ian.derrington@gmail.com>
2018-11-13 07:45:37 -0800

    better generalization, argument handling and cleanup

--- color_controller.py
+++ color_controller.py
@@ -26,18 +26,34 @@ class ColorDesigner(object):
     def __init__(self, color_mat):
         self.color_mat = color_mat
         self.n_lights = color_mat.shape[0]
+        self.parser = argparse.ArgumentParser(description="Make Single Color")
+        self.set_args()
         self.get_args()
         self.process_args()
+        self.cleanup_args()
     
-    def get_arg(self):
-        p = argparse.ArgumentParser(description="Highest Class")
+    def set_args(self):
+        pass
+
+    def get_args(self):
+
+        args, unused_args = self.parser.parse_known_args()
+        if unused_args is not None:
+            msg = 'Warning! Unused/unrecognized arguments: {}'.format(unused_args)
+            print(msg)
+        self.args = vars(args)
 
     def process_args(self):
         pass
+    
+    def cleanup_args(self):
+        del self.parser
 
     def update_color_mat(self):
-        pass
-
+        for light_ndx in range(self.n_lights):
+            for color_ndx, color_val in enumerate(self.color):
+                self.color_mat[light_ndx, color_ndx] = GPIO.HIGH * color_val
+    
     def run(self):
         self.update_color_mat()
 
@@ -47,27 +63,15 @@ class MakeMatrixColor(ColorDesigner):
     def __init__(self, color_mat):
         super(MakeMatrixColor, self).__init__(color_mat)
 
-    def get_args(self):
-        p = argparse.ArgumentParser(description="Make Single Color")
-        p.add_argument("-c", "--color",
+    def set_args(self):
+        self.parser.add_argument("-c", "--color",
                 type=str, choices=COLOR_MAP.keys(), default='r',
                 help='the rgb opcy option')
 
-        args, unused_args= p.parse_known_args()
-        if unused_args is not None:
-            msg = 'Warning! Unused/unrecognized arguments: {}'.format(unused_args)
-            print(msg)
-        self.args = vars(args)
-    
     def process_args(self):
         if isinstance(self.args['color'], str):
             color = COLOR_MAP[self.args['color']]
         self.color = color
-
-    def update_color_mat(self):
-        for light_ndx in range(self.n_lights):
-            for color_ndx, color_val in enumerate(self.color):
-                self.color_mat[light_ndx, color_ndx] = GPIO.HIGH * color_val
 
 
 class MakePWMColor(MakeMatrixColor):
@@ -75,9 +79,9 @@ class MakePWMColor(MakeMatrixColor):
     def __init__(self, color_mat):
         super(MakePWMColor, self).__init__(color_mat)
 
-    def get_args(self):
-        p = argparse.ArgumentParser(description="PWM color")
-        p.add_argument("-f",'--frequency',
-                type=float
+    def set_args(self):
+        self.p.add_argument("-f",'--frequency',
+                type=float, default = 100.0,
+                help='1 / arbitrary time units')
 
 #: Add to light controller a 'run_until' node. This will allow it to run until a specified time and then pass information to the next light controller. 

187d29e7e039aae63ebe3492efa6ed3f54c30e9e
Ian Derrington <33096391+ideaian@users.noreply.github.com>
2018-11-15 12:59:50 -0800

    Create args_from_cmd.py

---
+++ args_from_cmd.py
@@ -0,0 +1,25 @@
+
+class ArgsFromCMD(object):
+    #: base class for other ways to make lights
+    def __init__(self, description='Default description'):
+        self.parser = argparse.ArgumentParser(description=description)
+        self.description = description
+        self.set_args()
+        self.get_args()
+        self.process_args()
+        self.cleanup_args()
+
+    def get_args(self):
+        args, unused_args = self.parser.parse_known_args()
+        if unused_args is not None:
+            msg = 'Warning! Parser described as {} Unused/unrecognized arguments: {}'.format(self.description, unused_args)
+            print(msg)
+        self.args = vars(args)
+        
+    def set_args(self):
+        pass
+
+    def process_args(self):
+        pass
+    
+    def cleanup_args(self):

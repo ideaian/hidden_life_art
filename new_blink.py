439b27569dd49b274c2b95b7b6f49b03abf97955
Ian Derrington <33096391+ideaian@users.noreply.github.com>
2018-11-15 16:09:28 -0800

    Update new_blink.py

--- new_blink.py
+++ new_blink.py
@@ -6,7 +6,7 @@ import threading
 
 
 from light_controller import (
-        LightController
+        LightController, PWMLightController
 )
 from color_controller import (
          MakePWMColor, MakeMatrixColor, COLOR_MAP
@@ -16,6 +16,9 @@ from color_controller import (
 AVAILABLE_COLOR_DESIGNERS = {'one_color': MakeMatrixColor,
                              'pwm_color': MakePWMColor
                              }
+
+AVAILABLE_LIGHT_CONTROLLERS = {'solid': LightController,
+                     'pwm': PWMLightController}
 PINOUT_MATRIX = \
         np.array([[25, 24, 23],
                   [16, 21, 20],
@@ -28,9 +31,11 @@ def get_args():
     import argparse
 
     p = argparse.ArgumentParser(description="Make colors")
-
-    p.add_argument("--color_designer", required=True,
-            type=str, choices=AVAILABLE_COLOR_DESIGNERS.keys(), default=None,
+    p.add_argument("--light_controller",
+            type=str, choices=AVAILABLE_LIGHT_CONTROLLERS.keys(), default='solid',
+            help="One of the available color designer functions in the list {}".format(AVAILABLE_LIGHT_CONTROLLERS.keys()))
+    p.add_argument("--color_designer",
+            type=str, choices=AVAILABLE_COLOR_DESIGNERS.keys(), default='one_color',
             help="One of the available color designer functions in the list {}".format(AVAILABLE_COLOR_DESIGNERS.keys()))
     args, _ = p.parse_known_args() 
 
@@ -39,15 +44,18 @@ def get_args():
 
 def get_color_designer(args):
     cd = AVAILABLE_COLOR_DESIGNERS[args['color_designer']]
-    
     return cd
 
+def get_light_controller(args)
+    lc = AVAILABLE_LIGHT_CONTROLLERS[args['light_controller']]
+    return lc
 
 def main():
     
     args = get_args()
     color_designer = get_color_designer(args)
-    gcw = LightController(
+    light_controller = get_light_controller(args)
+    gcw = light_controller(
             pinout = PINOUT_MATRIX,
             gpio_mode = 'bcm',
             color_designer = color_designer.run,
